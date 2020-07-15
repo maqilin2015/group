183e768556087049d55c5ad18eb429ce6ace0deb
maqilin2015 <2972402269@qq.com>
2020-07-15 23:02:40 +0800

    1

--- src/main/java/edu/tanzhou/group/Application.java
+++ src/main/java/edu/tanzhou/group/Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-	// 第一次提交master11
-	// 第一次提交 branch11
+	// 第一次提交b
+	// 第一次提交b
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
