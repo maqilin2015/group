41d62f6be664fd3007d4908133cb77a465f8a9c2
maqilin2015 <2972402269@qq.com>
2020-07-25 22:52:42 +0800

    1

--- src/main/java/edu/tanzhou/group/User.java
+++
@@ -1,7 +0,0 @@
-package edu.tanzhou.group;
-
-public class User {
-    public static void main(String[] args) {
-        System.out.println("main");
-    }
-}
--- src/main/java/edu/tanzhou/group/a.java
+++
@@ -1,7 +0,0 @@
-package edu.tanzhou.group;
-
-public class a {
-    public static void main(String[] args) {
-        
-    }
-}
